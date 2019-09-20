35cbdc244293c4887374fc5c9a93aedc425cfc8b
diesilveira <53955407+diesilveira@users.noreply.github.com>
2019-09-20 18:37:19 -0300

    agregue clas twitter image y agregue lo de wpp y twitter a program

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -35,6 +35,25 @@ namespace Library
             mensaje4.Send(mensaje4);
             // Enviar un MD en Twitter a algunos contactos
             
+            /*
+            WHATSAPP
+            const string accountSid = "ACa8395e7223e7719988c09db8dfb68f0f";
+            const string authToken = "fdcaac12dc7e5b031908429ee402e7a4";
+
+            var whatsApp = new WhatsAppApi(accountSid, authToken);
+            string sid = whatsApp.Send("+598<Poné tu teléfono acá>", "Hey there! I'm using WhatsAppUCU");
+            Console.WriteLine(sid);
+
+            TWITTER
+            string consumerKey = "g7rkPB5uI2xOqELAhlNrorSU4";
+            string consumerKeySecret = "8hOTyS71GrTH9Ool3rXykAJRY5AmgSPiy78b1wYUPcvfIzXeEc";
+            string accessTokenSecret = "eAut3eKlWwBB0o1BfTRLaRBfOgXF6WriMKwpkevgf7C2t";
+            string accessToken = "1396065818-9Q6o38qm99WQywFeqrJFTfs7DFAhI4LvjoJvFRk";
+            var twitter = new TwitterImage(consumerKey, consumerKeySecret, accessToken, accessTokenSecret);
+            Console.WriteLine(twitter.PublishToTwitter("New Employee 2! ",@"bill2.jpg"));
+            var twitterDirectMessage = new TwitterMessage(consumerKey, consumerKeySecret, accessToken, accessTokenSecret);
+            Console.WriteLine(twitterDirectMessage.SendMessage("Hola!", "380889967"));
+            */
         
         }
     }
